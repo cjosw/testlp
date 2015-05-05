9e44a19e83ba45e59bd4c0959654e8337e7879fa
Conor O'Neill <conor.oneill@softwire.com>
2015-05-05 15:58:07 +0100

    Added missing semicolons

--- main/src/webapp/WEB-INF/scripts/main.js
+++ main/src/webapp/WEB-INF/scripts/main.js
@@ -1,21 +1,21 @@
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function LearningPlanViewModel() {
-    this.user = ko.observable()
-    this.progress = ko.observable()
-    this.initial_plan = ko.observable()
-    this.category_plan = ko.observable()
-    this.errorMessage = ko.observable()
+    this.user = ko.observable();
+    this.progress = ko.observable();
+    this.initial_plan = ko.observable();
+    this.category_plan = ko.observable();
+    this.errorMessage = ko.observable();
 }
 
 function startup() {
-    rootUrl = 'https://dev.kallidus.com/DevEval/LMS/Handlers/ApiProxy.ashx/'
-    userId = '80380afd-42fa-4baa-a8c1-66ff2c8799d7'
+    rootUrl = 'https://dev.kallidus.com/DevEval/LMS/Handlers/ApiProxy.ashx/';
+    userId = '80380afd-42fa-4baa-a8c1-66ff2c8799d7';
     coursesPerRow = 6;
 
-    learning_plan = new LearningPlanViewModel()
+    learning_plan = new LearningPlanViewModel();
     setupKnockoutCustomisations();
-    ko.applyBindings(learning_plan)
+    ko.applyBindings(learning_plan);
 
-    loadUser(userId)
-    loadTrainingPlan(userId)
+    loadUser(userId);
+    loadTrainingPlan(userId);
 }
