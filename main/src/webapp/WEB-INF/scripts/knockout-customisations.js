aedfa6f3b80bbef05953c2aca3900ab3fefeab66
Conor O'Neill <conor.oneill@softwire.com>
2015-05-06 14:39:12 +0100

    Attempt to scroll to the newly opened panel

--- main/src/webapp/WEB-INF/scripts/knockout-customisations.js
+++ main/src/webapp/WEB-INF/scripts/knockout-customisations.js
@@ -19,10 +19,13 @@ function setupKnockoutCustomisations() {
             var animationClassname, defaultDuration, elementFullyHidden, elementFullyVisible, value;
             animationClassname = "se-fully-visible";
             elementFullyVisible = function() {
-                return $(element).addClass(animationClassname);
+                $(element).addClass(animationClassname);
+                if (!isElementInViewport(element)) {
+                    scrollToElement(element);
+                }
             };
             elementFullyHidden = function() {
-                return $(element).removeClass(animationClassname);
+                $(element).removeClass(animationClassname);
             };
             value = ko.utils.unwrapObservable(valueAccessor());
             defaultDuration = 400;
@@ -33,4 +36,23 @@ function setupKnockoutCustomisations() {
             }
         }
     };
+}
+
+function scrollToElement(element) {
+    var el = $(element);
+    if (el.length > 0) {
+        el[0].scrollIntoView({block: "start", behavior: "smooth"});
+    }
+}
+
+function isElementInViewport (el) {
+
+    var rect = el.getBoundingClientRect();
+    var inViewport = (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+    return inViewport;
 }
