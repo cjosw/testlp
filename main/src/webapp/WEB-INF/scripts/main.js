9aa5845fdf4067c6dbb7b38b3f330e44ac717d4e
Conor O'Neill <conor.oneill@softwire.com>
2015-05-14 13:29:30 +0100

    Use a generic 'person' rather than the Mona Lisa photo

--- main/src/webapp/WEB-INF/scripts/main.js
+++ main/src/webapp/WEB-INF/scripts/main.js
@@ -36,7 +36,7 @@ function decodeQueryParams(options) {
     userId     = decodeQueryParam(params, "userId",     options.userId, '{CurrentUser}');
     rootAPIUrl = decodeQueryParam(params, "rootAPIUrl", options.rootAPIUrl, 'https://dev.kallidus.com/DevEval/LMS/Handlers/ApiProxy.ashx/');
     rootUIUrl  = decodeQueryParam(params, "rootUIUrl",  options.rootUIUrl, 'http://dev.kallidus.com/DevEval/LMS/');
-    rootContentUrl  = decodeQueryParam(params, "rootContentUrl",  options.rootContentUrl, 'https://rawgit.com/cjosw/testlp/grid/main/src/webapp/WEB-INF/');
+    rootContentUrl  = decodeQueryParam(params, "rootContentUrl",  options.rootContentUrl, '');
     coursesPerRow        = decodeIntQueryParam(params,     "coursesPerRow",        options.coursesPerRow, 6);
     useDummyUser         = decodeBooleanQueryParam(params, "useDummyUser",         options.useDummyUser, "false");
     useRealTrainingData  = decodeBooleanQueryParam(params, "useRealTrainingData",  options.useRealTrainingData, "true");
